7d1e331d16f255fccad4384f1049c8628aa2c3b5
Tássio Jordão <tassio72@hotmail.com>
2019-06-21 11:36:48 -0300

    page coffee

--- index.js
+++ index.js
@@ -16,16 +16,44 @@ app.get('/', (req, res) => {
 });
 
 // article
-app.get('/voce-sabe-como-organizar-um-coffee-break-para-eventos', (req, res) => {
-    res.sendFile(__dirname + '/public/articles/voce-sabe-como-organizar-um-coffee-break-para-eventos.html')
+app.get('/como-montar-um-coffee-break-para-eventos', (req, res) => {
+    res.sendFile(__dirname + '/public/articles/como-montar-um-coffee-break-para-eventos.html')
 });
 
+app.get('/dicas-para-esquentas-parada-orgulho-lgbt', (req, res) => {
+    res.sendFile(__dirname + '/public/articles/dicas-para-esquentas-parada-orgulho-lgbt.html')
+});
+
+app.get('/guia-para-planejamento-de-jogos-universitarios', (req, res) => {
+    res.sendFile(__dirname + '/public/articles/guia-para-planejamento-de-jogos-universitarios.html')
+});
+
+app.get('/como-preparar-uma-palestra-de-sucesso', (req, res) => {
+    res.sendFile(__dirname + '/public/articles/como-preparar-uma-palestra-de-sucesso.html')
+});
+
+app.get('/corpus-christi-sp-2019', (req, res) => {
+    res.sendFile(__dirname + '/public/articles/corpus-christi-sp-2019.html')
+});
+
+app.get('/guia-para-organizar-festa-junina', (req, res) => {
+    res.sendFile(__dirname + '/public/articles/guia-para-organizar-festa-junina.html')
+});
+
+app.get('/passo-a-passo-evento-corporativo', (req, res) => {
+    res.sendFile(__dirname + '/public/articles/passo-a-passo-evento-corporativo.html')
+});
+
+
+// get leads
 app.post('/leads', (req, res) => {
     const teste = req.body;
     const lead = Lead.newLead(teste);
     res.send("Obrigado por se cadastrar. Vamos te tirar do tédio! ;)")
 });
 
+
+// get csv
 app.get('/mailing-list-csv', (req, res) => {
     res.setHeader('Content-Type', 'text/csv');
     res.setHeader('Content-Disposition', 'attachment: filename\"' + 'mailing.csv"');
